4b18f9f28ac6d11fead61ba69176b98438c5286b
Teo <teo4mido@gmail.com>
2022-09-19 11:31:08 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a class="underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p><a class="underline" href="https://tailwindcss.com/docs/guides/sveltekit">TailwindCSS</a></p>
